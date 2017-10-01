ca1524a170a3f6388b27134962122ed83a40b983
sevenleo <enfimleo@gmail.com>
2017-10-01 20:02:57 -0300

    instalar.py

---
+++ nltk/instalar.py
@@ -0,0 +1,7 @@
+import nltk
+nltk.download('mac_morpho')
+nltk.download('floresta')
+nltk.download('machado')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('stopwords')
