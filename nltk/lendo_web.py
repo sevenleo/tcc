1fd9bdef3444e5b158f35de6e4d366eab4c340f7
sevenleo <enfimleo@gmail.com>
2017-10-04 07:08:50 -0300

    ler paginas da web

--- nltk/lendo_web.py
+++ nltk/lendo_web.py
@@ -13,8 +13,6 @@ soup = BeautifulSoup(page, 'html.parser')
 
 #busca a primeira ocorrencia com find ou busca todas com findAll
 noticias = soup.findAll('a', attrs={'class': 'hui-premium__link'})
-i=1
 for n in noticias:
-	print("Noticia ["+str(i)+"] "+n.text.strip())
-	i=i+1
+	print("Noticia ["+str(noticias.index(n)+1)+"] "+n.text.strip())
 
