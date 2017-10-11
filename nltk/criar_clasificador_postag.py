1f20a3f00ff68893b3a4a65970021e577c14b680
sevenleo <enfimleo@gmail.com>
2017-10-10 23:14:32 -0300

    classificador pessoal

--- nltk/criar_clasificador_postag.py
+++ nltk/criar_clasificador_postag.py
@@ -1,28 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: latin-1 -*- 
 
+print("Incializando")
 #import unidecode
 #unaccented_string = unidecode.unidecode(txt)
 import pickle
 import nltk
 from nltk.corpus import mac_morpho, stopwords
-
-
-
-
-
 lang = 'portuguese'
-train = mac_morpho.tagged_sents()
-#train = nltk.corpus.floresta.tagged_sents()
 
+
+
+
+print("Carregango base")
+#train = mac_morpho.tagged_sents()
+#train = nltk.corpus.floresta.tagged_sents()
+json_data=open('wiki.json').read()
+train = json.loads(json_data)
+
+
+
+
+print("Separando arquivos test/treino")
 #train = mac_morpho.tagged_sents()[100:]
 #test = mac_morpho.tagged_sents()[:100]
 
 
 
-
-
-
+print("Treinando taggers")
 # http://nlpforhackers.io/training-pos-tagger/
 # DefaultTagger that simply tags everything with the same tag
 # RegexpTagger that applies tags according to a set of regular expressions
@@ -40,13 +45,12 @@ tag3 = nltk.TrigramTagger(train, backoff=tag2)
 
 
 
-
+print("Verificando acuracia")
 #https://streamhacker.com/2008/11/03/part-of-speech-tagging-with-nltk-part-1/
 #verficar a precisao no NLTK
 #nltk.tag.accuracy(tag3, test)
 #verficar a precisao no NLTK 2.0
 #tag3.evaluate(test)
-
 
 '''
 #ADICONAR ESTES TREINOS
@@ -54,8 +58,6 @@ tag3 = nltk.TrigramTagger(train, backoff=tag2)
 dataset1 = list(nltk.corpus.floresta.tagged_sents())
 dataset2 = [[w[0] for w in sent] for sent in nltk.corpus.mac_morpho.tagged_paras()]
 '''
-
-
 '''
 #ADICIONAR LISTA PALAVRAS MANUALMENTE
 	palavras desconhecidas (unk) devem ser adicionadas manualmente
@@ -74,5 +76,12 @@ dataset2 = [[w[0] for w in sent] for sent in nltk.corpus.mac_morpho.tagged_paras
 
 
 #SAVE TRAIN FILE
-file_tag3 = open('tag3_mac.obj', 'w') 
+filename = 'tag3_wiki.obj'
+print("Salvando arquivo "+filename)
+file_tag3 = open(filename, 'w') 
 pickle.dump(tag3, file_tag3) 
+
+
+
+
+print("Base finalizada")
