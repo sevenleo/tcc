10c2cc7de9e8991be8ca8c6c70e13b0a925d6227
sevenleo <enfimleo@gmail.com>
2017-11-21 18:45:15 +0000

    arquivos base para a instalacao inicial

---
+++ instalar_parte1.py
@@ -0,0 +1,54 @@
+import os
+os.system("clear all")
+
+print("___________ sudo apt-get update ___________")
+os.system("sudo apt-get update")
+
+
+print("___________ sudo apt-get install -y python-pip ___________")
+os.system("sudo apt-get install -y python-pip")
+
+
+print("___________ sudo pip install --upgrade pip ___________")
+os.system("sudo pip install --upgrade pip")
+
+
+print("___________ sudo pip install setuptools ___________")
+os.system("sudo pip install setuptools")
+
+
+print("___________ sudo pip install nltk ___________")
+os.system("sudo pip install nltk")
+
+
+print("___________ sudo pip install unidecode ___________")
+os.system("sudo pip install unidecode")
+
+
+print("___________ sudo pip install BeautifulSoup4 ___________")
+os.system("sudo pip install BeautifulSoup4")
+
+
+print("___________ sudo pip install terminaltables ___________")
+os.system("sudo pip install terminaltables")
+
+
+print("___________ sudo pip install --upgrade gensim ___________")
+os.system("sudo pip install --upgrade gensim")
+
+
+print("___________ sudo pip install scipy ___________")
+os.system("sudo pip install scipy")
+
+
+print("___________ sudo pip install numpy ___________")
+os.system("sudo pip install numpy")
+
+
+print("___________ sudo pip install scikit-learn ___________")
+os.system("sudo pip install scikit-learn")
+
+
+print("___________ pip freeze > requirements.txt ___________")
+os.system("pip freeze > requirements.txt")
+
---
+++ instalar_parte2.py
@@ -0,0 +1,13 @@
+import nltk
+print('Instalando modulos do NLTK ... ')
+
+print('mac_morpho')
+nltk.download('mac_morpho')
+print('floresta')
+nltk.download('floresta')
+print('machado')
+nltk.download('machado')
+print('punkt')
+nltk.download('punkt')
+print('stopwords')
+nltk.download('stopwords')
