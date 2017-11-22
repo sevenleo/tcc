166e9088fb0138816cbf48ca9c2d65d1b283cfec
sevenleo <enfimleo@gmail.com>
2017-11-22 10:25:12 +0000

    word2vec lendo arquivo

--- word2vec/test03.py
+++ word2vec/test03.py
@@ -1,40 +1,48 @@
 
 #https://radimrehurek.com/gensim/models/word2vec.html
+#https://codesachin.wordpress.com/2015/10/09/generating-a-word2vec-model-from-a-block-of-text-using-gensim-python/
+#https://radimrehurek.com/gensim/models/doc2vec.html
 
 #IMPORT
 print("_________ IMPORT _________")
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 from gensim.models import Word2Vec
+
 import pickle
-
-#CREATE W2V
-print("_________ CREATE W2V _________")
-#sentences = LineSentence('myfile.txt')
-#sentences = LineSentence('compressed_text.txt.bz2')
-#sentences = LineSentence('compressed_text.txt.gz')
-file='text8'
-sentences = Word2Vec.LineSentence('texts/'+file)
-text8 = Word2Vec(sentences)
-
-#SAVE FILES
-print("_________  FILES SAVE _________")
-filename1 = 'W2V/text8.W2V.objb'
-file1 = open(filename1, 'wb') 
-pickle.dump(mac, file1) 
+file = 'text8'
+load = True
 
 
-#LOAD FILES
-print("_________  FILES LOAD _________")
-#mac = pickle.load(open('W2V/mac.W2V.objb', "rb"))
-#flor = pickle.load(open('W2V/flor.W2V.objb', "rb"))
+if load == False:
+	#CREATE W2V
+	print("_________ CREATE W2V :"+file+"_________")
+	
+	from gensim.models.word2vec import LineSentence
+	##sentences = LineSentence('myfile.txt')
+	##sentences = LineSentence('compressed_text.txt.bz2')
+	##sentences = LineSentence('compressed_text.txt.gz')
 
+	sentences = LineSentence('texts/'+file)
+	w2v = Word2Vec(sentences)
+
+
+	#SAVE FILES
+	savefilename = 'W2V/'+file+'.W2V.objb'
+	print("_________  SAVE FILE :"+savefilename+" _________")
+	savefile = open(savefilename, 'wb') 
+	pickle.dump(w2v, savefile) 
+
+else:
+	#LOAD FILES
+	print("_________  LOAD FILE :"+file+"_________")
+	w2v = pickle.load(open('W2V/'+file+'.W2V.objb', "rb"))
 
 #USES
-print("_________ USES _________")
+print("_________ EXEMPLOS _________")
 print("DOG")
-print ( text8.most_similar('dog', topn=5) )
+print ( w2v.most_similar('dog', topn=5) )
 print("LOS ANGELES")
-print ( text8.most_similar('los_angeles', topn=5) )
+print ( w2v.predict_output_word(['los','angeles']) )
 print("LOS ANGELES")
-print ( text8.predict_output_word(['los','angeles']) )
+print ( w2v.most_similar('chicago', topn=5) )
