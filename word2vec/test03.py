369ef86476c8e33209be99de76a6be8ca0174845
sevenleo <enfimleo@gmail.com>
2017-11-22 17:06:59 +0000

    word2vec tentando adicionar conteudo

--- word2vec/test03.py
+++ word2vec/test03.py
@@ -13,10 +13,28 @@ def logs(titulo,texto="",texto2=""):
 	log = log + "\n---------------------------------------\n"
 	print (log)
 
+def tokenize(frase):
+    tokens=[]
+    lang = "portuguese"
+    for word in nltk.word_tokenize(frase, lang):
+            #tokens.append(word.decode("utf-8"))
+            tokens.append(word)
+    return tokens
 
+def sentenizer(text):
+	sentences = nltk.sent_tokenize(text)
+	return sentences
+
+def cleantext(text):
+	text = re.sub(r'[\.]', '', text)
+	text = re.sub(r"[']", '', text)
+	text = re.sub(r"ˈ", '', text)
+	return text
 
 #IMPORT
 logs("IMPORT")
+import nltk
+import re
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 from gensim.models import Word2Vec
@@ -24,7 +42,7 @@ from gensim.models import Word2Vec
 import pickle
 file = 'text8'
 load = True
-
+onlinesearch=True
 
 if load == False:
 	#CREATE W2V
@@ -62,3 +80,29 @@ logs("PREDICT")
 print("LOS ANGELES")
 print ( w2v.predict_output_word(['los','angeles']) )
 
+
+#NEW SENTENCES
+logs("NEW SENTENCES","with Wikipedia","http://wikipedia.readthedocs.io/en/latest/quickstart.html")
+newword = 'xuxa'
+
+if onlinesearch:
+	import wikipedia
+	#wikipedia.set_lang("pt")
+	results = wikipedia.search(newword)
+	page = wikipedia.page(results[0])
+	#text = wikipedia.summary(newword)
+	text = page.content
+	text = cleantext(text)
+	new_sentences = sentenizer(text)
+
+else:
+	new_sentences = LineSentence('texts/'+newword)
+print(text)
+print(len(new_sentences))
+print(len(new_sentences))
+print(len(new_sentences))
+print(new_sentences)
+w2v.build_vocab(new_sentences, update=True)
+w2v.train(new_sentences)
+print ( w2v.most_similar(newword, topn=5) )
+print ( w2v.predict_output_word([newword]) )
