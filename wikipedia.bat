8cd17bf2bb1eb2065dcaa9b88c0bed8629c96b3b
Leonardo Neves <leoneves@eq.ufrj.br>
2024-07-22 19:13:42 -0300

    wikipedia

--- wikipedia.bat
+++ wikipedia.bat
@@ -3,7 +3,7 @@ REM Ativa o ambiente virtual
 call venv\Scripts\activate.bat
 
 REM Muda o diretório para a pasta desejada
-cd /d "word2vec/CLUEWEB"
+cd /d "word2vec/WIKIPEDIA"
 
 REM Mantém o terminal aberto
 cmd /k
